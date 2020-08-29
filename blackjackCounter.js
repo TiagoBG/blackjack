89fa7e40115be99a9c84da390925f59a895d31a5
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-08-29 18:45:32 -0500

    correcion de la carga de las cartas y el funcionamiento de la baraja

--- blackjackCounter.js
+++ blackjackCounter.js
@@ -4,11 +4,9 @@ var espacio = document.getElementById("table");
 var table = espacio.getContext("2d");
 
 var mesa = {
-    url: "blackjackTable.jpg",
+    url: "imagenes/blackjackTable.jpg",
     cargaOK: false
 }
-
-
 
 mesa.imagen = new Image();
 mesa.imagen.src = mesa.url;
@@ -27,29 +25,46 @@ var card1;
 var card2;
 
 var cards = [{
-        url: "/imagenes/2-pica.jpg",
-        value: 2
-    }, {
-        url: "/imagenes/3-pica.jpg",
-        value: 3
-    }, {
-        url: "/imagenes/4-pica.jpg",
-        value: 4
-    }, {
-        url: "/imagenes/5-pica.jpg",
-        value: 5
-    }, {
-        url: "/imagenes/6-pica.jpg",
-        value: 6
-    }, {
-        url: "/imagenes/7-pica.jpg",
-        value: 7
-    }, {
-        url: "/imagenes/8-pica.jpg",
-        value: 8
-    },
-    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-];
+    url: "imagenes/2-pica.jpg",
+    value: 2
+}, {
+    url: "imagenes/3-pica.jpg",
+    value: 3
+}, {
+    url: "imagenes/4-pica.jpg",
+    value: 4
+}, {
+    url: "imagenes/5-pica.jpg",
+    value: 5
+}, {
+    url: "imagenes/6-pica.jpg",
+    value: 6
+}, {
+    url: "imagenes/7-pica.jpg",
+    value: 7
+}, {
+    url: "imagenes/8-pica.jpg",
+    value: 8
+}, {
+    url: "imagenes/9-pica.jpg",
+    value: 9
+}, {
+    url: "imagenes/10-pica.jpg",
+    value: 10
+}, {
+    url: "imagenes/J-pica.jpg",
+    value: "J"
+}, {
+    url: "imagenes/Q-pica.jpg",
+    value: "Q"
+}, {
+    url: "imagenes/K-pica.jpg",
+    value: "K"
+}, {
+    url: "imagenes/A-pica.jpg",
+    value: "A"
+}];
+
 
 
 //BARAJAR CARTAS
@@ -58,30 +73,30 @@ var player1 = [cards[Math.floor(Math.random(0, 11) * 13)], cards[Math.floor(Math
 
 
 //ASIGNACIÓN DE VARIABLES
-if (player1[0] == "J" || player1[0] == "Q" || player1[0] == "K") {
+if (player1[0].value == "J" || player1[0].value == "Q" || player1[0].value == "K") {
     card1 = 10;
 }
 
-if (player1[1] == "J" || player1[1] == "Q" || player1[1] == "K") {
+if (player1[1].value == "J" || player1[1].value == "Q" || player1[1].value == "K") {
     card2 = 10;
 }
 
-if (player1[0] <= 10 && player1[0] >= 2) {
-    card1 = parseInt(player1[0]);
+if (player1[0].value <= 10 && player1[0].value >= 2) {
+    card1 = player1[0].value;
 }
 
-if (player1[1] <= 10 && player1[1] >= 2) {
-    card2 = parseInt(player1[1]);
+if (player1[1].value <= 10 && player1[1].value >= 2) {
+    card2 = player1[1].value;
 }
 
-if (player1[0] == "A") {
+if (player1[0].value == "A") {
     card1 = 11;
 }
 
-if (player1[1] == "A") {
+if (player1[1].value == "A") {
     card2 = 11;
 }
-
+console.log(player1[0].value, player1[1].value);
 document.write("PLAYER 1 has: " + card1 + ", " + card2);
 
 
