01cff26ef4249b33e594300f3afbd278d1b6dfd5
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-08-29 19:51:39 -0500

    Cartas ordenadas, un mensaje mas claro y el juego funcionando (aun falta pintar las cartas en la mesa)

--- blackjackCounter.js
+++ blackjackCounter.js
@@ -1,4 +1,63 @@
-//TRAER LA MESA
+//DEFINIR VARIABLES Y LA BARAJA DE CARTAS
+var card1;
+var card2;
+
+var cards = [{
+    url: "imagenes/2-pica.jpg",
+    value: 2,
+    cargaOK: false
+}, {
+    url: "imagenes/3-diamante.jpg",
+    value: 3,
+    cargaOK: false
+}, {
+    url: "imagenes/4-corazon.jpg",
+    value: 4,
+    cargaOK: false
+}, {
+    url: "imagenes/5-trebol.jpg",
+    value: 5,
+    cargaOK: false
+}, {
+    url: "imagenes/6-pica.jpg",
+    value: 6,
+    cargaOK: false
+}, {
+    url: "imagenes/7-diamante.jpg",
+    value: 7,
+    cargaOK: false
+}, {
+    url: "imagenes/8-corazon.jpg",
+    value: 8,
+    cargaOK: false
+}, {
+    url: "imagenes/9-trebol.jpg",
+    value: 9,
+    cargaOK: false
+}, {
+    url: "imagenes/10-pica.jpg",
+    value: 10,
+    cargaOK: false
+}, {
+    url: "imagenes/J-diamante.jpg",
+    value: "J",
+    cargaOK: false
+}, {
+    url: "imagenes/Q-corazon.jpg",
+    value: "Q",
+    cargaOK: false
+}, {
+    url: "imagenes/K-trebol.jpg",
+    value: "K",
+    cargaOK: false
+}, {
+    url: "imagenes/A-pica.jpg",
+    value: "A",
+    cargaOK: false
+}];
+
+
+//CARGAR LA MESA Y LAS CARTAS
 
 var espacio = document.getElementById("table");
 var table = espacio.getContext("2d");
@@ -10,69 +69,43 @@ var mesa = {
 
 mesa.imagen = new Image();
 mesa.imagen.src = mesa.url;
-mesa.imagen.addEventListener("load", cargarMesa);
+mesa.imagen.addEventListener("load", cargarJuego);
 
-function cargarMesa() {
+//REVISAR COMO CARGAR LAS CARTAS
+/*cards.imagen = new Image();
+cards.imagen.src = cards[0].url;
+cards.imagen.addEventListener("load", cargarJuego);
+
+cards.imagen = new Image();
+cards.imagen.src = cards[1].url;
+cards.imagen.addEventListener("load", cargarJuego);*/
+
+
+//BARAJAR Y REPARTIR CARTAS
+var player1 = [cards[Math.floor(Math.random(0, 11) * 13)], cards[Math.floor(Math.random(0, 11) * 13)]];
+
+console.log(player1[0].cargaOK, player1[1].cargaOK);
+
+function cargarJuego() {
     mesa.cargaOK = true;
+    cards.carga
     if (mesa.cargaOK == true) {
         table.drawImage(mesa.imagen, 0, 0);
+    }
+    player1[0].cargaOK = true;
+    player1[1].cargaOK = true;
+
+    console.log(player1[0].cargaOK, player1[1].cargaOK);
+    console.log(player1[0].value, player1[1].value);
+
+    if (cards.carga) {
+
     }
 }
 
 
-//DEFINIR VARIABLES Y LA BARAJA DE CARTAS
-var card1;
-var card2;
 
-var cards = [{
-    url: "imagenes/2-pica.jpg",
-    value: 2
-}, {
-    url: "imagenes/3-pica.jpg",
-    value: 3
-}, {
-    url: "imagenes/4-pica.jpg",
-    value: 4
-}, {
-    url: "imagenes/5-pica.jpg",
-    value: 5
-}, {
-    url: "imagenes/6-pica.jpg",
-    value: 6
-}, {
-    url: "imagenes/7-pica.jpg",
-    value: 7
-}, {
-    url: "imagenes/8-pica.jpg",
-    value: 8
-}, {
-    url: "imagenes/9-pica.jpg",
-    value: 9
-}, {
-    url: "imagenes/10-pica.jpg",
-    value: 10
-}, {
-    url: "imagenes/J-pica.jpg",
-    value: "J"
-}, {
-    url: "imagenes/Q-pica.jpg",
-    value: "Q"
-}, {
-    url: "imagenes/K-pica.jpg",
-    value: "K"
-}, {
-    url: "imagenes/A-pica.jpg",
-    value: "A"
-}];
-
-
-
-//BARAJAR CARTAS
-var player1 = [cards[Math.floor(Math.random(0, 11) * 13)], cards[Math.floor(Math.random(0, 11) * 13)]];
-
-
-
-//ASIGNACIÓN DE VARIABLES
+//ASIGNACIÓN DE PUNTOS
 if (player1[0].value == "J" || player1[0].value == "Q" || player1[0].value == "K") {
     card1 = 10;
 }
@@ -96,16 +129,16 @@ if (player1[0].value == "A") {
 if (player1[1].value == "A") {
     card2 = 11;
 }
-console.log(player1[0].value, player1[1].value);
-document.write("PLAYER 1 has: " + card1 + ", " + card2);
+var sum = card1 + card2;
+document.write("PLAYER 1 has this cards: " + player1[0].value + " and " + player1[1].value + ". That means you have: " + card1 + " + " + card2 + " (" + sum + ") points");
 
 
 //LÓGICA DEL JUEGO BLACKJACK
-if (card1 + card2 > 21) {
+if (sum > 21) {
     document.write(" - YOU LOSE");
-} else if (card1 + card2 == 21) {
+} else if (sum == 21) {
     document.write(" - YOU WON");
-} else if (card1 + card2 > 16) {
+} else if (sum > 16) {
     document.write(" - HOLD ON");
 } else {
     document.write(" - TAKE ANOTHER CARD");
